58efc89432a74a763b68345b86e629378e628c9e
ZaksenCode <maksimsidorkin2006@mail.ru>
2024-07-14 11:34:15 +0300

    Initial commit

---
+++ src/main/java/me/zaksen/noname/NoName.java
@@ -0,0 +1,10 @@
+package me.zaksen.noname;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NoName implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/zaksen/noname/mixin/PlayerEntityMixin.java
@@ -0,0 +1,33 @@
+package me.zaksen.noname.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.scoreboard.AbstractTeam;
+import net.minecraft.scoreboard.Team;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class PlayerEntityMixin {
+
+    @Shadow public abstract ServerWorld getServerWorld();
+
+    @Inject(at = @At("HEAD"), method = "onSpawn")
+    private void onSpawn(CallbackInfo ci) {
+        PlayerEntity player = (PlayerEntity) (Object) this;
+
+        if(this.getServerWorld().getScoreboard().getTeam("players_team") == null) {
+            Team playersTeam = this.getServerWorld().getScoreboard().addTeam("players_team");
+            playersTeam.setNameTagVisibilityRule(AbstractTeam.VisibilityRule.NEVER);
+            playersTeam.setShowFriendlyInvisibles(false);
+        }
+
+        Team playersTeam = this.getServerWorld().getScoreboard().getTeam("players_team");
+        this.getServerWorld().getScoreboard().addScoreHolderToTeam(player.getNameForScoreboard(), playersTeam);
+    }
+
+}
